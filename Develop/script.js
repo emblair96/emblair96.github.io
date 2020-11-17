45163f0963473085e49cd11488369c7b9bd7440e
Emily Blair <emilyblair96@gmail.com>
2020-11-16 19:35:34 -0600

    Update styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,17 @@
 var projectCard = document.querySelector(".card");
+var introSubtitle = document.querySelector(".intro-subtitle")
+
+var dataText = "An aspiring software developer."
+
+function typeWriter() {
+    for (var i=0; i<dataText.length; i++) {
+        if (i<dataText.length) {
+            introSubtitle.textContent += dataText[i]
+    }
+
+}
+
+}
+
+//typeWriter();
 
