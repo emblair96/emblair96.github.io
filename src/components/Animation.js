b08c83e37dd2b6719efa25fa249107c9aeb78177
Emily Blair <emilyblair96@gmail.com>
2021-03-03 18:22:47 -0600

    Add project links

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -4,7 +4,7 @@ import test from '../images/sync.svg'
 import LocalFloristIcon from '@material-ui/icons/LocalFlorist';
 import { Grid, Typography, Divider, Icon } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faBicycle, faDog, faHiking, faPuzzlePiece, faPodcast } from '@fortawesome/free-solid-svg-icons'
+import { faLaptop, faCoffee, faBicycle, faDog, faHiking, faPuzzlePiece, faPodcast, faPlane } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Animation() {
@@ -12,14 +12,14 @@ export default function Animation() {
     <div>
 
       <h3 style={{textAlign: "center"}}>What I do when I'm not coding:</h3>
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faCoffee} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faBicycle} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faDog} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faHiking} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faPuzzlePiece} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faPodcast} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faDog} />
-      <FontAwesomeIcon className="fa-4x bigEntrance hobbie-icons" icon={faDog} />
+      <FontAwesomeIcon className="fa-4x tossing hobbie-icons" icon={faLaptop} />
+      <FontAwesomeIcon className="fa-4x floating hobbie-icons" icon={faCoffee} />
+      <FontAwesomeIcon className="fa-4x tossing hobbie-icons" icon={faBicycle} />
+      <FontAwesomeIcon className="fa-4x floating hobbie-icons" icon={faDog} />
+      <FontAwesomeIcon className="fa-4x floating hobbie-icons" icon={faHiking} />
+      <FontAwesomeIcon className="fa-4x tossing hobbie-icons" icon={faPuzzlePiece} />
+      <FontAwesomeIcon className="fa-4x floating hobbie-icons" icon={faPodcast} />
+      <FontAwesomeIcon className="fa-4x tossing hobbie-icons" icon={faPlane} />
 
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import { Grid, Container } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import '../App.css';
 import { ThemeProvider } from '@material-ui/styles';
+import '../animations.css'
+
 
 export default function Header({ theme2 }) {
   return (
@@ -13,7 +15,7 @@ export default function Header({ theme2 }) {
         alignItems="center"
       >
         <ThemeProvider theme={theme2}>
-          <Typography variant="h1"> Hi, I'm Emily.</Typography>
+          <Typography variant="h1" className="bigEntrance"> Hi, I'm Emily.</Typography>
         </ThemeProvider>
         <Typography variant="h5"> A full stack software engineer.</Typography>
       </Grid>
