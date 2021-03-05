0a91a5fa3a7484c23d460c558dedd9a3942600a3
Emily Blair <emilyblair96@gmail.com>
2021-03-05 00:48:13 -0600

    Add working navbar btns & additional mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState, refs, useRef } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,29 +15,52 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { Container } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
-import {theme, theme2} from './theme';
+import { theme, theme2 } from './theme';
 import './App.css';
 
 function App() {
+  const [selected, setSelected] = useState()
+  const selectedBtn = useRef(null);
+  const handleBtnClick = (href) => {
+    setSelected(href)
+    refs.selected.scrollIntoView()
+  }
+
+  useEffect(() => {
+    if (selected && selectedBtn.current) {
+      selectedBtn.current.scrollIntoView();
+      setSelected(false);
+    }
+  }, [selected, selectedBtn.current]);
+
+
+  useEffect(() => console.log('selected', selected))
+
   return (
     <Router>
-    <div className="App" style={{ backgroundColor: "#cfd8dc" }}>
-      <Container>
+      <div className="App" style={{ backgroundColor: "#cfd8dc" }}>
+        <Container>
           <ThemeProvider theme={theme}>
-            <Navbar theme={theme} />
-            <Header theme2={theme2}/>
+            <Link to={selected}>
+              <Navbar theme={theme} onClickContact={() => setSelected(true)} btnRef={selectedBtn} />
+            </Link>
+            <div id="home"></div>
+            <Header id="home" theme2={theme2} />
             <Subheader theme2={theme2} text="About Me" />
-              <AboutMe theme={theme} />
+            <AboutMe theme={theme} />
             <Subheader theme2={theme2} text="My Developer Journey" />
-              <DeveloperJourney />
+            <DeveloperJourney />
+            <div id="projects"></div>
             <Subheader theme2={theme2} text="My Work" id="project" />
-              <MyWork />
-            <Subheader theme2={theme2} text="Contact Me" />
-              <Contact theme2={theme2} />
+            <MyWork />
+            <div id="contact"></div>
+            <Subheader theme2={theme2} text="Contact Me"  />
+            <Contact theme2={theme2} />
+
             <Footer />
           </ThemeProvider>
-      </Container>
-    </div>
+        </Container>
+      </div>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import {AppBar, Button, Container, Grid, Toolbar} from '@material-ui/core';
+import { AppBar, Button, Container, Grid, Toolbar } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
-
+import {Link} from 'react-scroll';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -19,38 +19,47 @@ function ElevationScroll(props) {
 }
 
 
-export default function Navbar(props) {
+export default function Navbar(props, ref) {
+
   return (
     <ThemeProvider theme={props.theme}>
-    <React.Fragment>
-      <CssBaseline />
-      <ElevationScroll {...props}>
-        <AppBar style={{backgroundColor: "#cfd8dc"}}>
-          <Container>
-            
-            <Grid
-              container
-              direction="row"
-              justify="flex-end"
-              alignItems="center"
-            >
-              <Toolbar>
-                <Button className="btn" style={{margin: ".3em", fontSize: "1.2em"}} variant="outlined" href="#outlined-buttons">
-                  Home
-                </Button>
-                <Button style={{margin: ".3em", fontSize: "1.2em"}} variant="outlined" href="#projects">
-                  Projects
-                </Button>
-                <Button style={{margin: ".3em", fontSize: "1.2em"}} variant="outlined" href="#outlined-buttons">
-                  Contact
-                </Button>
-              </Toolbar>
-            </Grid>
-          </Container>
-        </AppBar>
-      </ElevationScroll>
-      <Toolbar />
-    </React.Fragment>
+      <React.Fragment>
+        <CssBaseline />
+        <ElevationScroll {...props}>
+          <AppBar style={{ backgroundColor: "#cfd8dc" }}>
+            <Container>
+
+              <Grid
+                container
+                direction="row"
+                justify="flex-end"
+                alignItems="center"
+              >
+
+                <Toolbar>
+                  
+                  <Link to="home">
+                    <Button className="btn" style={{ margin: ".3em", fontSize: "1.2em" }} variant="outlined" to="#home"> 
+                      Home
+                    </Button>
+                  </Link>
+                  <Link to="projects">
+                    <Button style={{ margin: ".3em", fontSize: "1.2em" }} variant="outlined" href="#projects">
+                      Projects
+                    </Button>
+                  </Link>
+                  <Link to="contact">
+                    <Button style={{ margin: ".3em", fontSize: "1.2em" }} variant="outlined" href="#contact">
+                      Contact
+                    </Button>
+                  </Link>
+                </Toolbar>
+              </Grid>
+            </Container>
+          </AppBar>
+        </ElevationScroll>
+        <Toolbar />
+      </React.Fragment>
     </ThemeProvider>
   );
 }
